ce7d010d470dd186238b77d91321803629337b3e
Aydin Can <aydinjons@users.noreply.github.com>
2018-09-10 18:26:57 +0300

    arrays

--- deneme.ts
+++ deneme.ts
@@ -1,15 +1,21 @@
-let a : string;
-let b : number;
-let c : boolean;
-let d : any;
+// let a : string;
+// let b : number;
+// let c : boolean;
+// let d : any;
 
-a = "hello world";
-b = 22;
-c = true;
+// a = "hello world";
+// b = 22;
+// c = true;
 
-console.log(a);
-console.log('naber');
-console.log(b);
-console.log(a,b);
-console.log(a,b,c);
-console.log('d');
+// console.log(a);
+// console.log('naber');
+// console.log(b);
+// console.log(a,b);
+// console.log(a,b,c);
+// console.log('d');
+
+let langs : string[];
+
+langs = ["php","java","c++","python"];
+
+console.log(langs);
